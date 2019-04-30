3b069ce2494f71e7fcd857a114afbc8871b5dae3
kain <zhangtqx@vip.qq.com>
2019-04-30 11:57:53 +0800

    Submit

--- src/queue.ts
+++ src/queue.ts
@@ -1,6 +1,27 @@
 import {Instance} from './types/instance';
+import {CreateQueue} from './queue/create-queue';
 
 export class Queue {
-    constructor(instance: Instance) {
+    constructor(private instance: Instance) {
+    }
+
+    CreateQueue(queueName: string,
+                maxMsgHeapNum?: number,
+                pollingWaitSeconds?: number,
+                visibilityTimeout?: number,
+                maxMsgSize?: number,
+                msgRetentionSeconds?: number,
+                rewindSeconds?: number) {
+        const action = new CreateQueue(this.instance, {
+            Action: 'CreateQueue',
+            queueName,
+            maxMsgHeapNum,
+            pollingWaitSeconds,
+            visibilityTimeout,
+            maxMsgSize,
+            msgRetentionSeconds,
+            rewindSeconds
+        });
+        return action.result();
     }
 }
