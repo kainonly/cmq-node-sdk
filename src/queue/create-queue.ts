35c4b57b2c63f0140eb06e8e95c627d689a876b4
kain <kainpro@foxmail.com>
2019-04-30 11:57:53 +0800

    Submit

--- src/queue/create-queue.ts
+++ src/queue/create-queue.ts
@@ -4,7 +4,6 @@ import {Instance} from '../types/instance';
 
 export class CreateQueue extends Common {
     constructor(instance: Instance, options: CreateQueueOptions) {
-        options.Action = 'CreateQueue';
         super(instance, options, 'queue');
     }
 }
