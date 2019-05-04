444254ff3e94dbc083a064f2e30e9a21792ae3e3
kain <zhangtqx@vip.qq.com>
2019-05-04 11:48:39 +0800

    创建主题

--- src/topic.ts
+++ src/topic.ts
@@ -1,6 +1,23 @@
 import {Instance} from "./types/instance";
+import {CreateTopic} from "./queue/create-topic";
 
 export class Topic {
     constructor(private instance: Instance) {
+    }
+
+    /**
+     * 创建主题
+     * @param topicName 主题名字
+     * @param maxMsgSize 消息最大长度
+     * @param filterType 用于指定主题的消息匹配策略
+     * @constructor
+     */
+    CreateTopic(topicName: string, maxMsgSize?: number, filterType?: number) {
+        return new CreateTopic(this.instance, {
+            Action: 'CreateTopic',
+            topicName,
+            maxMsgSize,
+            filterType
+        });
     }
 }
