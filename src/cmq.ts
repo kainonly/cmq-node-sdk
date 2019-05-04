777d0b1baa3ad004c22f2301b5465ca157b0623a
kain <kainpro@foxmail.com>
2019-05-04 10:21:32 +0800

    Develop

--- src/cmq.ts
+++ src/cmq.ts
@@ -1,5 +1,6 @@
 import {Instance} from './types/instance';
 import {Queue} from './queue';
+import {Topic} from "./topic";
 
 export namespace CMQ {
     /**
@@ -15,8 +16,18 @@ export namespace CMQ {
         constructor(private instance: Instance) {
         }
 
+        /**
+         * 初始化队列操作
+         */
         queue() {
             return new Queue(this.instance);
+        }
+
+        /**
+         * 初始化主题操作
+         */
+        topic() {
+            return new Topic(this.instance);
         }
     }
 }
