f95e1dc982db83bd7d90302b792018f925f510d3
kain <zhangtqx@vip.qq.com>
2019-05-11 18:33:42 +0800

    新增泛型

--- src/types/queue/batch-send-message-response.ts
+++ src/types/queue/batch-send-message-response.ts
@@ -1,9 +1,13 @@
 import {CommonResponse} from "../common-response";
-import {SendMessageResponse} from "./send-message-response";
 
 export interface BatchSendMessageResponse extends CommonResponse {
     /**
      * 服务器生成消息的唯一标识 ID 列表，每个元素是一条消息的信息。
      */
-    msgList: SendMessageResponse[];
+    msgList: {
+        /**
+         * 服务器生成消息的唯一标识 ID。
+         */
+        msgId: string
+    }[];
 }
