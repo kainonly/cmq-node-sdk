62ae37d03afcf3bab8365f6037dd1c5579fb9918
kain <kainpro@foxmail.com>
2019-05-11 19:45:31 +0800

    预定义单元测试

---
+++ testing/queue-message.spec.ts
@@ -0,0 +1,35 @@
+import {ok, fail} from 'assert';
+import {config} from 'dotenv';
+import {CMQ} from '../../cmq-nodejs-sdk';
+
+const env = config().parsed;
+const cmq = CMQ.NEW({
+    path: '/v2/index.php',
+    signatureMethod: 'HmacSHA256',
+    extranet: true,
+    secretId: env.secretId,
+    secretKey: env.secretKey,
+    region: 'gz'
+});
+
+describe('Test Queue', () => {
+    it('Create Queue ', function () {
+
+    });
+
+    it('Send Message', function () {
+
+    });
+
+    it('Receive Delete Message', function () {
+
+    });
+
+    it('Batch Send Message', function () {
+
+    });
+
+    it('Batch Receive Delete Message', function () {
+
+    });
+});
