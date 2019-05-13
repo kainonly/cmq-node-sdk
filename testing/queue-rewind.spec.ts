d28132a15f9cacd2663b491f7883ef5209012f8b
kain <kainpro@foxmail.com>
2019-05-13 09:09:42 +0800

    单元测试与优化

--- testing/queue-rewind.spec.ts
+++ testing/queue-rewind.spec.ts
@@ -14,18 +14,58 @@ const cmq = CMQ.NEW({
 
 describe('Test Queue Rewind', () => {
     it('Create Queue', async () => {
-
+        try {
+            const res = await cmq.createQueue({
+                queueName: 'beta',
+                maxMsgHeapNum: 1000000,
+                rewindSeconds: 3600
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
     it('Send Message', async () => {
-
+        try {
+            const res = await cmq.sendMessage({
+                queueName: 'beta',
+                msgBody: {
+                    name: 'kain'
+                }
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
     it('Receive And Delete Message ', async () => {
-
+        try {
+            const res1 = await cmq.receiveMessage({
+                queueName: 'beta'
+            });
+            if (res1.code !== 0) fail(res1.message);
+            const res2 = await cmq.deleteMessage({
+                queueName: 'beta',
+                receiptHandle: res1.receiptHandle
+            });
+            ok(res2.code === 0, res2.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
     it('Rewind Queue', async () => {
-
+        try {
+            const time = parseInt((new Date().getTime() / 1000).toString());
+            const res = await cmq.rewindQueue({
+                queueName: 'beta',
+                startConsumeTime: time - 1800
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 });
