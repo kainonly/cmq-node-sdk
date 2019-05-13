d05028226a2dad6955f2b290ef7a851347d7768b
kain <zhangtqx@vip.qq.com>
2019-05-13 09:40:12 +0800

    单元测试与优化

--- testing/topic-subscribe.spec.ts
+++ testing/topic-subscribe.spec.ts
@@ -13,31 +13,89 @@ const cmq = CMQ.NEW({
 });
 
 describe('Test Topic Subscribe', () => {
-    it('Create Topic', () => {
-
+    it('Create Topic', async () => {
+        try {
+            const res = await cmq.createTopic({
+                topicName: 'sub-topic'
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
-    it('Subscribe', () => {
-
+    it('Subscribe', async () => {
+        try {
+            const res = await cmq.subscribe({
+                topicName: 'sub-topic',
+                subscriptionName: 'test',
+                protocol: 'queue',
+                endpoint: 'normal',
+                filterTag: ['mytag']
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
-    it('List Subscription By Topic', () => {
-
+    it('List Subscription By Topic', async () => {
+        try {
+            const res = await cmq.listSubscriptionByTopic({
+                topicName: 'sub-topic'
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
-    it('Set Subscription Attributes', () => {
-
+    it('Set Subscription Attributes', async () => {
+        try {
+            const res = await cmq.setSubscriptionAttributes({
+                topicName: 'sub-topic',
+                subscriptionName: 'test',
+                notifyStrategy: 'BACKOFF_RETRY'
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
-    it('Unsubscribe', () => {
-
+    it('Unsubscribe', async () => {
+        try {
+            const res = await cmq.unsubscribe({
+                topicName: 'sub-topic',
+                subscriptionName: 'test'
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
-    it('Get Subscription Attributes', () => {
-
+    it('Get Subscription Attributes', async () => {
+        try {
+            const res = await cmq.getSubscriptionAttributes({
+                topicName: 'sub-topic',
+                subscriptionName: 'test'
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 
-    it('Clear Subscription Filter Tags', () => {
-
+    it('Clear Subscription Filter Tags', async () => {
+        try {
+            const res = await cmq.clearSubscriptionFilterTags({
+                topicName: 'sub-topic',
+                subscriptionName: 'test'
+            });
+            ok(res.code === 0, res.message);
+        } catch (e) {
+            fail(e);
+        }
     });
 });
