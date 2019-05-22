6f37b8916025f2b2c1a921ad9debaa624cafd313
kain <zhangtqx@vip.qq.com>
2019-05-22 17:51:38 +0800

    更新自动测试

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,6 @@ const del = require('del');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
 
-// mocha --require ts-node/register --ui bdd testing\queue.spec.ts
-
 gulp.task('default', function () {
     del.sync(['dist']);
     return gulp.src('src/**/*.ts')
