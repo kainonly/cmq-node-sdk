bc5f91213b2eb583b7bd1f032c6c3693ae52e0ae
kain <zhangtqx@vip.qq.com>
2019-12-13 17:40:17 +0800

    更新自动测试与覆盖率计算

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,10 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '!**/node_modules/**',
+    'dist/*.js',
+    '!dist/types/**',
+  ],
 };
