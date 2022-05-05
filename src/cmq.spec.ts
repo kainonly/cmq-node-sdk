569982bb1fbc8708b77876bccf9fff8fa24cb08f
kain <kainpro@foxmail.com>
2022-05-06 01:00:02 +0800

    TDMQ CMQ 开发测试

--- src/cmq.spec.ts
+++ src/cmq.spec.ts
@@ -2,21 +2,79 @@ import { env } from 'process';
 import { CMQ } from './';
 
 describe('HTTP 数据流测试', () => {
-  let cmq: CMQ;
+  let client: CMQ;
   beforeAll(() => {
-    cmq = new CMQ({
-      endpoint: 'cmq-gz.public.tencenttdmq.com',
+    client = new CMQ({
+      host: 'cmq-gz.public.tencenttdmq.com',
       secretId: env.SECRETID,
       secretKey: env.SECRETKEY,
       region: env.REGION
     });
   });
 
-  test('向队列发送消息', async () => {
-    const response = await cmq.sendMessage({
+  test('发布消息', async () => {
+    const r = await client.sendMessage({
       queueName: 'dev',
-      msgBody: `{"name":"kain"}`
+      msgBody: `{"msg":"hi"}`
     });
-    expect(response.code).toBe(0);
+    expect(r.code).toBe(0);
+  });
+
+  let receiptHandle: string;
+
+  test('消费消息', async () => {
+    const r = await client.receiveMessage({
+      queueName: 'dev',
+      pollingWaitSeconds: 15
+    });
+    expect(r.code).toBe(0);
+    expect(r.msgBody).toBe(`{"msg":"hi"}`);
+    expect(r.receiptHandle).not.toBeNull();
+    receiptHandle = r.receiptHandle;
+  });
+
+  test('删除消息', async () => {
+    const r = await client.deleteMessage({
+      queueName: 'dev',
+      receiptHandle
+    });
+    expect(r.code).toBe(0);
+  });
+
+  const data: string[] = [];
+
+  test('批量发送消息', async () => {
+    for (let i = 0; i < 3; i++) {
+      data.push(`{"index":${i}}`);
+    }
+    const r = await client.batchSendMessage({
+      queueName: 'batch-dev',
+      msgBody: data
+    });
+    expect(r.code).toBe(0);
+  });
+
+  const receiptHandles: string[] = [];
+
+  test('批量接收消息', async () => {
+    const r = await client.batchReceiveMessage({
+      queueName: 'batch-dev',
+      numOfMsg: 3,
+      pollingWaitSeconds: 15
+    });
+    expect(r.code).toBe(0);
+    expect(r.msgInfoList).not.toBeNull();
+    r.msgInfoList.forEach(value => {
+      expect(data).toContain(value.msgBody);
+      receiptHandles.push(value.receiptHandle);
+    });
+  });
+
+  test('批量删除消息', async () => {
+    const r = await client.batchDeleteMessage({
+      queueName: 'batch-dev',
+      receiptHandle: receiptHandles
+    });
+    expect(r.code).toBe(0);
   });
 });
--- src/cmq.ts
+++ src/cmq.ts
@@ -1,6 +1,25 @@
 import { createHmac } from 'crypto';
-import { Dto, Options, SendMessageDto, SendMessageResult } from './types';
-import axios from 'axios';
+import {
+  BatchDeleteMessageDto,
+  BatchDeleteMessageResult,
+  BatchPublishMessageDto,
+  BatchPublishMessageResult,
+  BatchReceiveMessageDto,
+  BatchReceiveMessageResult,
+  BatchSendMessageDto,
+  BatchSendMessageResult,
+  DeleteMessageDto,
+  Dto,
+  Options,
+  PublishMessageDto,
+  PublishMessageResult,
+  ReceiveMessageDto,
+  ReceiveMessageResult,
+  Result,
+  SendMessageDto,
+  SendMessageResult
+} from './types';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export class CMQ {
   /**
@@ -24,40 +43,135 @@ export class CMQ {
   /**
    * 发起数据流请求
    */
-  send<T, R>(dto: T & Dto): Promise<R> {
-    dto.SignatureMethod = 'HmacSHA256';
-    dto.SecretId = this.options.secretId;
-    dto.Region = this.options.region;
-    dto.Nonce = Math.trunc(Math.random() * 10000);
-    dto.Timestamp = Math.trunc(new Date().getTime() / 1000);
+  send<T, R>(data: T & Dto, config?: AxiosRequestConfig): Promise<R> {
+    data.SignatureMethod = 'HmacSHA256';
+    data.SecretId = this.options.secretId;
+    data.Region = this.options.region;
+    data.Nonce = Math.trunc(Math.random() * 10000);
+    data.Timestamp = Math.trunc(new Date().getTime() / 1000);
     const params: string[] = [];
-    for (const key of Object.keys(dto).sort()) {
-      params.push(`${key.replace('_', '.')}=${Reflect.get(dto, key)}`);
+    for (const key of Object.keys(data).sort()) {
+      params.push(`${key.replace(/_/g, '.')}=${Reflect.get(data, key)}`);
     }
-    dto.Signature = createHmac('sha256', this.options.secretKey)
-      .update(`POST${this.options.endpoint}${this.path}?${params.join('&')}`)
+    data.Signature = createHmac('sha256', this.options.secretKey)
+      .update(`POST${this.options.host}${this.path}?${params.join('&')}`)
       .digest('base64');
-    const data = new URLSearchParams();
-    for (const [key, value] of Object.entries(dto)) {
+    const payload = new URLSearchParams();
+    for (const [key, value] of Object.entries(data)) {
       if (Array.isArray(value)) {
         value.forEach((v, i) => {
-          data.append(`${key}.${i}`, v);
+          payload.append(`${key}.${i}`, v);
         });
       } else {
-        data.append(key, value);
+        payload.append(key, value);
       }
     }
-    return axios.post<R>(this.protocol + this.options.endpoint + this.path, data).then(response => {
-      return response.data;
-    });
+    return axios
+      .post<R>(this.protocol + this.options.host + this.path, payload, {
+        timeout: 30000,
+        ...config
+      })
+      .then(response => response.data);
   }
 
   /**
-   * 用于发送一条消息到指定的队列
-   * @param dto
+   * 发送消息
+   * @param data
    */
-  sendMessage(dto: SendMessageDto): Promise<SendMessageResult> {
-    dto.Action = 'SendMessage';
-    return this.send(dto);
+  sendMessage(data: SendMessageDto): Promise<SendMessageResult> {
+    data.Action = 'SendMessage';
+    return this.send(data);
+  }
+
+  /**
+   * 批量发送消息
+   * @param data
+   */
+  batchSendMessage(data: BatchSendMessageDto): Promise<BatchSendMessageResult> {
+    data.Action = 'BatchSendMessage';
+    data.msgBody.forEach((value, index) => {
+      Reflect.set(data, `msgBody.${index}`, value);
+    });
+    delete data.msgBody;
+    return this.send(data);
+  }
+
+  /**
+   * 设置超时
+   * @param data
+   * @private
+   */
+  private static pollingWaitTime(data: ReceiveMessageDto | BatchReceiveMessageDto): AxiosRequestConfig {
+    const cfg: AxiosRequestConfig = {};
+    if (data.pollingWaitSeconds) {
+      cfg.timeout = (data.pollingWaitSeconds + 1) * 1000;
+    }
+    return cfg;
+  }
+
+  /**
+   * 消费消息
+   * @param data
+   */
+  receiveMessage(data: ReceiveMessageDto): Promise<ReceiveMessageResult> {
+    data.Action = 'ReceiveMessage';
+    return this.send(data, CMQ.pollingWaitTime(data));
+  }
+
+  /**
+   * 批量消费消息
+   * @param data
+   */
+  batchReceiveMessage(data: BatchReceiveMessageDto): Promise<BatchReceiveMessageResult> {
+    data.Action = 'BatchReceiveMessage';
+    return this.send(data, CMQ.pollingWaitTime(data));
+  }
+
+  /**
+   * 删除消息
+   * @param data
+   */
+  deleteMessage(data: DeleteMessageDto): Promise<Result> {
+    data.Action = 'DeleteMessage';
+    return this.send(data);
+  }
+
+  /**
+   * 批量删除消息
+   * @param data
+   */
+  batchDeleteMessage(data: BatchDeleteMessageDto): Promise<BatchDeleteMessageResult> {
+    data.Action = 'BatchDeleteMessage';
+    data.receiptHandle.forEach((value, index) => {
+      Reflect.set(data, `receiptHandle.${index}`, value);
+    });
+    delete data.receiptHandle;
+    return this.send(data);
+  }
+
+  /**
+   * 发布消息
+   * @param data
+   */
+  publishMessage(data: PublishMessageDto): Promise<PublishMessageResult> {
+    data.Action = 'PublishMessage';
+    return this.send(data);
+  }
+
+  /**
+   * 批量发布消息
+   * @param data
+   */
+  batchPublishMessage(data: BatchPublishMessageDto): Promise<BatchPublishMessageResult> {
+    data.Action = 'BatchPublishMessage';
+    data.msgBody.forEach((value, index) => {
+      Reflect.set(data, `msgBody.${index}`, value);
+    });
+    delete data.msgBody;
+    data.msgTag?.forEach((value, index) => {
+      Reflect.set(data, `msgTag.${index}`, value);
+    });
+    delete data.msgTag;
+    return this.send(data);
   }
 }
